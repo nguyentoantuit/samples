d7e0f3403f247725af0d232a15685437021d7862
nguyentoanuit <nguyentoanuit@gmail.com>
2017-01-16 23:25:21 +0700

    Generate personal information(not completed yet)

--- GeneratePDF/Samples.GeneratePDF/PDFGenerator.cs
+++ GeneratePDF/Samples.GeneratePDF/PDFGenerator.cs
@@ -11,6 +11,34 @@ using System.IO;
 
 namespace Samples.GeneratePDF
 {
+    public class CommonStyle
+    {
+        public Style HeaderStyle { get; set; }
+        public Style TableHeaderStyle { get; set; }
+        public Style BoldStyle { get; set; }
+        public Style NormalStyle { get; set; }
+
+        public CommonStyle()
+        {
+            // Setup fonts
+            PdfFont boldDefaultFont = PdfFontFactory.CreateRegisteredFont(FontConstants.HELVETICA_BOLD);
+            PdfFont defaultFont = PdfFontFactory.CreateRegisteredFont(FontConstants.HELVETICA);
+            HeaderStyle = CreateStyle(boldDefaultFont, 18f);
+            TableHeaderStyle = CreateStyle(boldDefaultFont, 16f);
+            BoldStyle = CreateStyle(boldDefaultFont, 11f);
+            NormalStyle = CreateStyle(defaultFont, 11f);
+
+        }
+
+        private static Style CreateStyle(PdfFont font, float size)
+        {
+            Style style = new Style();
+            style.SetFont(font);
+            style.SetFontSize(size);
+            return style;
+        }
+    }
+
     public class PDFGenerator
     {
         private const string destinationPath = "D:\\Test.pdf";
@@ -29,14 +57,11 @@ namespace Samples.GeneratePDF
                 document.SetMargins(36, 36, 72, 36);
 
                 // Setup fonts
-                PdfFont boldDefaultFont = PdfFontFactory.CreateRegisteredFont(FontConstants.HELVETICA_BOLD);
-                PdfFont defaultFont = PdfFontFactory.CreateRegisteredFont(FontConstants.HELVETICA);
-                var headerStyle = CreateStyle(boldDefaultFont, 18f);
-                var tableHeaderStyle = CreateStyle(boldDefaultFont, 16f);
-                var boldStyle = CreateStyle(boldDefaultFont, 11f);
-                var normalStyle = CreateStyle(defaultFont, 11f);
+                CommonStyle commonStyle = new CommonStyle();
 
-                GenerateHeader(document, headerStyle);
+                GenerateHeader(document, commonStyle);
+
+                GeneratePersonalInfo(document, commonStyle);
 
                 //Close document
                 document.Close();
@@ -47,7 +72,56 @@ namespace Samples.GeneratePDF
 
         }
 
-        private static void GenerateHeader(Document document, Style titleStyle)
+        private static void GeneratePersonalInfo(Document document, CommonStyle commonStyle)
+        {
+            float[] widths = new float[] { 2.5f, 3.5f, 2.5f, 3.5f };
+            Table table = CreatePdfTable(widths);
+
+            //Heading
+            Cell cellTitle = new Cell(1, widths.Length).Add(new Paragraph("Personal Information").AddStyle(commonStyle.TableHeaderStyle));
+            cellTitle.SetBorder(Border.NO_BORDER);
+            cellTitle.SetHorizontalAlignment(HorizontalAlignment.LEFT);
+            table.AddCell(cellTitle);
+            table.AddCell(CreateTableCellSeperator(widths.Length));
+
+            table.AddCell(new Cell().Add(new Paragraph("FirstName").AddStyle(commonStyle.BoldStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("Toan").AddStyle(commonStyle.NormalStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("LastName").AddStyle(commonStyle.BoldStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("Nguyen").AddStyle(commonStyle.NormalStyle)).SetBorder(Border.NO_BORDER));
+
+            table.AddCell(new Cell().Add(new Paragraph("Email").AddStyle(commonStyle.BoldStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("nguyentoanuit@gmail.com").AddStyle(commonStyle.NormalStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("Phone Number").AddStyle(commonStyle.BoldStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("123 456 789").AddStyle(commonStyle.NormalStyle)).SetBorder(Border.NO_BORDER));
+
+            table.AddCell(CreateTableCellSeperator(widths.Length));
+
+            table.AddCell(new Cell().Add(new Paragraph("Gender").AddStyle(commonStyle.BoldStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("Male").AddStyle(commonStyle.NormalStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("Adress").AddStyle(commonStyle.BoldStyle)).SetBorder(Border.NO_BORDER));
+            table.AddCell(new Cell().Add(new Paragraph("Ho Chi Minh, Vietnam").AddStyle(commonStyle.NormalStyle)).SetBorder(Border.NO_BORDER));
+            
+            document.Add(table);
+        }
+
+        private static Table CreatePdfTable(float[] widths)
+        {
+            Table table = new Table(widths);
+            table.SetBorder(Border.NO_BORDER);
+            table.SetWidth(540f);
+
+            table.SetHorizontalAlignment(HorizontalAlignment.LEFT);
+            table.SetMarginTop(10f);
+            table.SetMarginBottom(10f);
+            return table;
+        }
+
+        private static Cell CreateTableCellSeperator(int colsSpan)
+        {
+            return new Cell(1, colsSpan).SetMarginBottom(10f).SetBorder(Border.NO_BORDER);
+        }
+
+        private static void GenerateHeader(Document document, CommonStyle commonStyle)
         {
             Table table = new Table(1);
             table.SetWidth(540f);
@@ -57,7 +131,7 @@ namespace Samples.GeneratePDF
             table.SetMarginBottom(25f);
             string headerTitle = string.Format("Test Header Title {0}", DateTime.Now);
             //Heading
-            Cell cellTitle = new Cell().Add(new Paragraph(headerTitle).AddStyle(titleStyle));
+            Cell cellTitle = new Cell().Add(new Paragraph(headerTitle).AddStyle(commonStyle.HeaderStyle));
             cellTitle.SetBorder(Border.NO_BORDER);
             cellTitle.SetHorizontalAlignment(HorizontalAlignment.LEFT);
             table.AddCell(cellTitle);
@@ -65,18 +139,9 @@ namespace Samples.GeneratePDF
             document.Add(table);
         }
 
-        private static Style CreateStyle(PdfFont font, float size)
-        {
-            Style style = new Style();
-            style.SetFont(font);
-            style.SetFontSize(size);
-            return style;
-        }
-
         private static void SaveStreamToDocument(string destinationPath, byte[] ms)
         {
             File.WriteAllBytes(destinationPath, ms);
         }
-
     }
 }
