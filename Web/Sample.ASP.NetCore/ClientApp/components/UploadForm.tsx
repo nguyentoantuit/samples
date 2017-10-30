08a2e1536c797b6fc5bf63747f7d8aa8e31e4851
IDEALOGIC\toan-huu.nguyen <toan-huu.nguyen@kolekto.nl>
2017-10-30 23:09:56 +0700

    Init react-dropzone

---
+++ Web/Sample.ASP.NetCore/ClientApp/components/UploadForm.tsx
@@ -0,0 +1,56 @@
+﻿import * as React from 'react';
+import * as Dropzone from 'react-dropzone';
+import * as stylesImport from './styles.scss';
+const styles: any = stylesImport;
+
+interface UploadFormProps {
+
+}
+
+interface UploadFormState {
+
+}
+
+class UploadForm extends React.Component<UploadFormProps, UploadFormState> {
+    private _filePicker: Dropzone;
+
+    constructor(props: UploadFormProps, context) {
+        super(props, context);
+        this._onDrop = this._onDrop.bind(this);
+    }
+
+    componentDidMount() {
+    }
+
+    componentWillReceiveProps(nextProps: UploadFormProps, nextContext?) {
+    }
+
+    private _onDrop(files: File[], rejected) {
+        if (files && files.length > 0) {
+            var name = files[0].name.split('.')[0];
+            console.log(`File name: ${name}`);
+        }
+    }
+
+    render() {
+        return (
+            <Dropzone
+                ref={ref => this._filePicker = ref}
+                accept="application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                className="file-picker"
+                rejectClassName="rejected-file"
+                acceptClassName="accepted-file"
+                onDrop={this._onDrop}
+                multiple={false}
+                disableClick={true}
+            >
+                <div className="form-group">
+                    <label htmlFor="usr">Name:</label>
+                    <input type="text" className="form-control" id="usr" />
+                </div>
+            </Dropzone>
+        );
+    }
+}
+
+export default UploadForm;
---
+++ Web/Sample.ASP.NetCore/ClientApp/routes.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import Home from './components/Home';
+import FetchData from './components/FetchData';
+import Counter from './components/Counter';
+import UploadForm from './components/UploadForm';
+
+export const routes = <Layout>
+    <Route exact path='/' component={Home} />
+    <Route path='/counter' component={Counter} />
+    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
+    <Route path='/uploadform' render={prop => (
+        <UploadForm />
+        )} />
+</Layout>;
