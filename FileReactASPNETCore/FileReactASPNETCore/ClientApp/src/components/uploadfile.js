1494f3982114735280cc09543b2efa844a4ea6b0
Toan Nguyen <toan-huu.nguyen@kolekto.nl>
2019-04-24 13:46:06 +0700

    Setup upload file example: client side => done

---
+++ FileReactASPNETCore/FileReactASPNETCore/ClientApp/src/components/uploadfile.js
@@ -0,0 +1,40 @@
+﻿import React, { Component } from 'react';
+
+export class UploadFile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { files: null }
+
+        this.handleFileChange = this.handleFileChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
+    }
+
+    _handleSubmit(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        let formData = new FormData();
+        for (let i = 0; i < this.state.files.length; i++) {
+            formData.append('files', this.state.files[i]);
+        }
+        fetch('/api/files', {
+            method: 'POST',
+            body: formData
+        }).then();
+        return false;
+    }
+
+    handleFileChange(event) {
+        this.setState({ files: event.target.files });
+    }
+
+    render() {
+        return (
+            <form>
+                <input type="file" multiple={true} name="pic" onChange={this.handleFileChange} />
+                <input type="submit" onClick={this._handleSubmit} />
+            </form>
+        );
+    }
+}
+
+export default UploadFile;
---
+++ FileReactASPNETCore/FileReactASPNETCore/Controllers/FileController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileReactASPNETCore.Controllers
+{
+    [Route("api/files")]
+    public class FileController
+    {
+
+    }
+}
